d6c55abbbadf462f9006620e772da31ad5cac594
iqbal <iqbal.safian@gmail.com>
2020-12-30 04:34:14 +0800

    add basic react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './components/Home';
+import AboutMe from './components/AboutMe';
+import NavBar from './components/NavBar';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => {
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <NavBar />
+                <Switch>
+                    <Route path="/about-me">
+                        <AboutMe />
+                    </Route>
+                    <Route path="/">
+                        <Home />
+                    </Route>
+                </Switch>
+            </div>
+        </BrowserRouter>
+    );
+};
 
 export default App;
---
+++ src/components/Home/home.tsx
@@ -0,0 +1,14 @@
+import React, { FunctionComponent } from 'react';
+
+const Home: FunctionComponent = () => {
+    return (
+        <div>
+            <h1>Home</h1>
+            <div className="name">Iqbal Safian</div>
+            <div>Full-Stack Software Engineer/Developer/Researcher</div>
+            <div>With 15 years ( + sleep + eat + wasting time ) of experience developing applications</div>
+        </div>
+    );
+};
+
+export default Home;
