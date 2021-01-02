baf2edf4f4ee0988c48d0407a8c89bd1719d98b2
iqbal <iqbal.safian@gmail.com>
2021-01-02 08:54:45 +0800

    fix font file permission

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,29 +1,31 @@
 import React, { FunctionComponent } from 'react';
 import { NavLink } from 'react-router-dom';
-
 import './NavBar.scss';
 
 const NavBar: FunctionComponent = () => {
     return (
-        <nav>
-            <ul>
-                <li>
-                    <NavLink to="/">Home</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/about-me">About Me</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/job-history">Job History</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/works">Works</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/contact">Contact</NavLink>
-                </li>
-            </ul>
-        </nav>
+        <div>
+            <div className="header-title">Iqbal&apos;s Chronicle</div>
+            <nav>
+                <ul>
+                    <li>
+                        <NavLink to="/">Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/about-me">About Me</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/job-history">Job History</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/works">Works</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/contact">Contact</NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     );
 };
 
