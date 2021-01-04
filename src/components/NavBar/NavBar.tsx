46834c1a5d5fe8b3d4990627aab6328ed10ebefa
iqbal <iqbal.safian@gmail.com>
2021-01-04 13:44:29 +0800

    add background theme

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -5,26 +5,30 @@ import './NavBar.scss';
 const NavBar: FunctionComponent = () => {
     return (
         <div>
-            <div className="header-title">Iqbal&apos;s Chronicle</div>
-            <nav>
-                <ul>
-                    <li>
-                        <NavLink to="/">Home</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/about-me">About Me</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/job-history">Job History</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/works">Works</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/contact">Contact</NavLink>
-                    </li>
-                </ul>
-            </nav>
+            <div className="header">
+                <div className="header-title">Iqbal&apos;s Chronicle</div>
+            </div>
+            <div className="navbar">
+                <nav>
+                    <ul>
+                        <li>
+                            <NavLink to="/">Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/about-me">About Me</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/job-history">Job History</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/works">Works</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/contact">Contact</NavLink>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
         </div>
     );
 };
