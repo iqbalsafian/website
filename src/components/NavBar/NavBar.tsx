b0cc3777546653bac94b0e116d2a6a865f55621f
iqbal <iqbal.safian@gmail.com>
2021-01-08 12:26:54 +0800

    use ts for axios and add random quote

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -4,30 +4,56 @@ import axios from 'axios';
 import moment from 'moment';
 import './NavBar.scss';
 
+interface IUtcDate {
+    currentDateTime: string;
+}
+
+interface IRandomQuote {
+    quoteText: string;
+}
+
 const NavBar: FunctionComponent = () => {
-    const [utcDate, setUtcDate] = useState({});
+    const [utcDate, setUtcDate] = useState<IUtcDate | null>(null);
+    const [randomQuote, setRandomQuote] = useState<IRandomQuote | null>(null);
 
     useEffect(() => {
-        async function getUtcDate() {
-            const result = await axios('http://worldclockapi.com/api/json/utc/now');
-            setUtcDate(result);
-            setUtcDate('2021-01-07T08:16Z');
-        }
-
+        const getUtcDate = async (): Promise<void> => {
+            return await axios.get<IUtcDate>('http://worldclockapi.com/api/json/utc/now').then((response) => {
+                setUtcDate(response?.data);
+            });
+        };
         getUtcDate();
+    }, []);
+
+    useEffect(() => {
+        const getRandomQuote = async (): Promise<void> => {
+            return await axios
+                .get<IRandomQuote>('https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json')
+                .then((response) => {
+                    setRandomQuote(response?.data);
+                    console.log(response?.data);
+                })
+                .catch((err) => console.log(err));
+        };
+        getRandomQuote();
     }, []);
 
     return (
         <div>
             <div className="header container">
                 <div className="row">
-                    <div className="col-sm-2"></div>
+                    <div className="col-sm-2">
+                        <div className="quote">
+                            {randomQuote?.quoteText}
+                            <span className="block">a</span>
+                        </div>
+                    </div>
                     <div className="col-sm-8">
                         <h1>Iqbal&apos;s Chronicle</h1>
                     </div>
                     <div className="col-sm-2">
                         <div className="dates">
-                            <span className="block">{moment(utcDate).format('MMMM Do YYYY')}</span>
+                            <span className="block">{moment(utcDate?.currentDateTime).format('MMMM Do YYYY')}</span>
                             <span className="block">since 1983</span>
                         </div>
                     </div>
