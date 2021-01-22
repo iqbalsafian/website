9224a0695eb796f9774f97ffbbec1a40ee4d3a5f
iqbal <iqbal.safian@gmail.com>
2021-01-22 12:50:47 +0800

    further decorate hamburgermenu

--- src/components/HamburgerMenu/HamburgerMenu.tsx
+++ src/components/HamburgerMenu/HamburgerMenu.tsx
@@ -9,7 +9,7 @@ const HamburgerMenu: FunctionComponent = () => {
     };
 
     return (
-        <div className={`container ${displayType}`} onClick={changeDisplayType}>
+        <div className={`container ${displayType} hamburger`} onClick={changeDisplayType}>
             <div className="bar1" />
             <div className="bar2" />
             <div className="bar3" />
